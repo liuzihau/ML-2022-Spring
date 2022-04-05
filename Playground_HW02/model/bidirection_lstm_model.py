ecfea93af63695bec71e154ca42b3dc050d269be
ubuntu <pokerliuzihau@gmail.com>
2022-04-05 20:04:47 +0800

    refactor HW02's code

--- Playground_HW02/model/bidirection_lstm_model.py
+++ Playground_HW02/model/bidirection_lstm_model.py
@@ -3,48 +3,53 @@ import torch
 
 
 class BasicBlock(nn.Module):
-    def __init__(self, input_dim, output_dim):
+    def __init__(self, input_dim, output_dim, dropout=True):
         super(BasicBlock, self).__init__()
-
-        self.block = nn.Sequential(
-            nn.Linear(input_dim, output_dim),
-            nn.ReLU(),
-        )
+        self.drop = dropout
+        self.hidden_size = output_dim
+        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.lstm = nn.LSTM(input_dim, output_dim, num_layers=1, batch_first=True,
+                            bidirectional=True, dropout=0)
+        self.layer_norm = nn.LayerNorm(2 * output_dim)
+        self.dropout = nn.Dropout(p=0.5)
 
     def forward(self, x):
-        x = self.block(x)
+        hidden = torch.zeros(1 * 2, x.size(0), self.hidden_size).to(self.device)
+        c = torch.zeros(1 * 2, x.size(0), self.hidden_size).to(self.device)
+        x, _ = self.lstm(x, (hidden, c))
+        x = self.layer_norm(x)
+        if self.drop:
+            x = self.dropout(x)
+
         return x
 
 
 class Classifier(nn.Module):
-    def __init__(self, input_dim, output_dim=41, hidden_layers=1, hidden_dim=256):
+    def __init__(self, input_dim, output_dim=41, hidden_layers=1, hidden_dim=256, seq=False):
         super(Classifier, self).__init__()
+        self.seq = seq
         self.input_dimension = int(39)
-        self.sequence_length = int(input_dim/39)
-        self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        self.sequence_length = int(input_dim / 39)
         self.hidden_size = hidden_dim
         self.num_layers = hidden_layers
-        self.lstm = nn.LSTM(self.input_dimension, hidden_dim, hidden_layers, batch_first=True,
-                            bidirectional=True)
-        self.fc = nn.Linear(hidden_dim * 2, 512)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 128)
-        self.fc4 = nn.Linear(128, output_dim)
+        self.basic_block = nn.Sequential(
+            BasicBlock(self.input_dimension, hidden_dim),
+            *[BasicBlock(2 * hidden_dim, hidden_dim) for _ in range(hidden_layers)],
+            BasicBlock(2 * hidden_dim, hidden_dim,
+                       dropout=False)
+        )
+
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
 
     def forward(self, x):
-        hidden = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
-        c = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_size).to(self.device)
+
         # out: tensor of shape (batch_size, seq_length, hidden_size)
         # out, hidden = self.rnn(x, hidden)
-        out, hidden = self.lstm(x, (hidden, c))
+        out = self.basic_block(x)
         # (batch_size, seq_length, hidden_size)-->(batch_size,hidden_size)
-        out = self.fc(out[:, -1, :])
-        out = self.relu(out)
-        out = self.fc2(out)
-        out = self.relu(out)
-        out = self.fc3(out)
-        out = self.relu(out)
-        out = self.fc4(out)
+        if self.seq:
+            out = self.fc(out.contiguous())
+        else:
+            out = self.fc(out[:, -1, :])
 
         return out
